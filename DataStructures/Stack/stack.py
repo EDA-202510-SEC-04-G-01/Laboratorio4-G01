85582f643280198701c6c54c6dde104bc007fa15
Navith10 <t.galarza@uniandes.edu.co>
2025-02-13 11:45:08 -0500

    primeros cambios1

--- DataStructures/Stack/stack.py
+++ DataStructures/Stack/stack.py
@@ -1,27 +1,27 @@
-from DataStructures.Stack import stack as st
+from DataStructures.List import single_linked_list as sl
 
 def new_stack():
-    stack = st.new_stack()
-    return stack
+    new_stack = sl.new_list()
+    return new_stack
 
 def push(stack, element):
-    stack = st.push(stack, element)
-    return stack
+    new_stack = sl.add_first(stack, element)
+    return new_stack
 
 def pop(stack):
-    stack = st.pop(stack)
-    return stack
+    new_stack = sl.remove_first(stack)
+    return new_stack
 
 def is_empty(stack):
-    rta = st.is_empty(stack)
+    rta = sl.is_empty(stack)
     return rta
 
 def top(stack):
-    top = st.top(stack)
+    top = sl.first_element(stack)
     return top
 
 def size(stack):
-    size = st.size(stack)
+    size = sl.size(stack)
     return size
 
     
